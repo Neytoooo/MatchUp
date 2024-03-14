8229f1166406087f0ce228cac0b564a6d3e670b9
Mattis <mattiskhn@gmail.com>
2024-03-14 16:26:22 +0100

    page notfound

---
+++ Client/app/src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>404</h1>
+      <h2>Page non trouvée</h2>
+      <p>Nous sommes désolés, mais la page que vous recherchez n'existe pas ou a été déplacée.</p>
+    </div>
+  );
+}
+
+export default NotFound;
