a7d1d133a37260bcf8cbe076264e60910d7093d4
Mattis <mattiskhn@gmail.com>
2024-03-14 16:44:22 +0100

    ajout

--- Client/app/src/App.jsx
+++ Client/app/src/App.jsx
@@ -1,47 +1,44 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 
-import Home from './components/Acceuil/Home/Home';
+import Home from './components/Acceuil/Home/Home.jsx';
+import Header from './components/Header/Header.jsx';
 import Login from './components/Authentification/LoginPage/Login';
 import Register from './components/Authentification/RegisterPage/Register';
 import UserProfile from './components/Authentification/UserProfile/Profile';
 import Tournaments from './components/Gestion_des_Tournois/Tournaments/Tournaments';
-import TournamentDetails from './components/Gestion_des_Tournois/TournamentDetails/TournamentDetails';
-// import NotFound from './components/NotFound';
+import CreateTournament from './components/Gestion_des_Tournois/CreateTournament/CreateTournament';
 
-// Un hook fictif pour déterminer si l'utilisateur est connecté
-// Vous devrez le remplacer par votre propre logique d'authentification.
+import TournamentDetails from './components/Gestion_des_Tournois/TournamentDetails/TournamentDetails';
+import NotFound from './components/NotFound';
+
 function useAuth() {
-  const user = { loggedIn: false }; // Exemple d'état d'utilisateur
-  return user && user.loggedIn;
+  const user = localStorage.getItem('user');
+  return user != null; 
 }
+
 
 function App() {
   const isAuth = useAuth();
 
   return (
     <Router>
+      
       <div>
-        <Switch>
-          {/* Redirige les utilisateurs non authentifiés vers la page Register */}
-          <Route exact path="/">
-            {isAuth ? <Home /> : <Redirect to="/register" />}
-          </Route>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          {/* Protège les routes nécessitant une authentification */}
-          <Route path="/user-profile">
-            {isAuth ? <UserProfile /> : <Redirect to="/login" />}
-          </Route>
-          <Route exact path="/tournaments">
-            {isAuth ? <Tournaments /> : <Redirect to="/login" />}
-          </Route>
-          <Route path="/tournaments/:id">
-            {isAuth ? <TournamentDetails /> : <Redirect to="/login" />}
-          </Route>
-          {/* Ajoutez d'autres routes ici au besoin */}
-          {/* <Route component={NotFound} /> */}
-        </Switch>
+      <Header />  
+        <Routes>
+          <Route path="/" element={isAuth ? <Home /> : <Navigate replace to="/register" />} />
+          <Route path="/home" element={isAuth ? <Home /> : <Navigate replace to="/register" />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/user-profile" element={isAuth ? <UserProfile /> : <Navigate replace to="/login" />} />
+          <Route exact path="/tournaments" element={isAuth ? <Tournaments /> : <Navigate replace to="/login" />} />
+          <Route path="/CreateTournament" element={isAuth ? <CreateTournament /> : <Navigate replace to="/login" />} />
+          <Route path="/tournaments/:id" element={isAuth ? <TournamentDetails /> : <Navigate replace to="/login" />} />
+          <Route path="/tournaments/:id" element={<TournamentDetails />} />
+
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
     </Router>
   );
