5d48d40954c89ed2b2517471065dd867e421c66e
Mattis <mattiskhn@gmail.com>
2024-03-14 16:47:48 +0100

    Modification model

--- Serveur/Script_Data/Create_Tournament.sql
+++ Serveur/Script_Data/Create_Tournament.sql
@@ -1,10 +1,14 @@
-CREATE TABLE IF NOT EXISTS Tournois (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255) NOT NULL,
-    jeuId INT,
-    typeTournoi ENUM('Public', 'Privé') NOT NULL,
-    cashPrice DECIMAL(10, 2),
-    dateDebut DATETIME,
-    dateFin DATETIME,
-    FOREIGN KEY (jeuId) REFERENCES Jeux(id)
+CREATE TABLE Tournois (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nom VARCHAR(255) NOT NULL,
+  jeu VARCHAR(255) NOT NULL,
+  nombreParticipants INT NOT NULL,
+  type ENUM('public', 'prive') NOT NULL,
+  parQui INT NOT NULL,
+  lienTwitch VARCHAR(255),
+  lienYoutube VARCHAR(255),
+  reseaux VARCHAR(255),
+  dateCreation DATETIME NOT NULL,
+  dateDebut DATETIME NOT NULL,
+  FOREIGN KEY (parQui) REFERENCES Utilisateurs(id)
 );
