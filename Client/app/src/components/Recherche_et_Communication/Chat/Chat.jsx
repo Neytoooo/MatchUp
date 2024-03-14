a686f0ced292e8d603b2d141f584952ac8747a8a
Mattis <mattiskhn@gmail.com>
2024-03-14 16:32:31 +0100

    Mise en place du Chat

---
+++ Client/app/src/components/Recherche_et_Communication/Chat/Chat.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+// Assurez-vous que l'adresse correspond à l'URL de votre serveur
+const socket = io('http://localhost:3001'); 
+
+function Chat() {
+  const [message, setMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    console.log("État actuel des messages :", messages);
+    const handleNewMessage = (msg) => {
+      console.log("Message reçu :", msg); // Log pour vérifier le message reçu
+      setMessages((messages) => [...messages, msg]);
+    };
+  
+    socket.on('chat message', handleNewMessage);
+  
+    return () => {
+      socket.off('chat message', handleNewMessage);
+    };
+  }, []);
+  
+  
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message.trim()) {
+      console.log("Envoi du message :", message); // Log pour vérifier le message envoyé
+      socket.emit('chat message', message);
+      setMessage('');
+    }
+  };
+  
+
+  return (
+    <div>
+      <h2>Chat en Direct</h2>
+      <ul>
+        {messages.map((msg, index) => (
+          <li key={index}>{msg}</li>
+        ))}
+      </ul>
+      <form onSubmit={sendMessage}>
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Tapez un message..."
+        />
+        <button type="submit">Envoyer</button>
+      </form>
+    </div>
+  );
+}
+
+export default Chat;
