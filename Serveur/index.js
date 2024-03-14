43b54df1df8d06c06be44158c640cd3c970bb80f
Mattis <mattiskhn@gmail.com>
2024-03-14 16:19:07 +0100

    Point d'Entrée Principal

--- Serveur/index.js
+++ Serveur/index.js
@@ -1,70 +1,29 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const http = require('http');
+const socketIo = require('socket.io');
+const app = require('./app'); // Assurez-vous que le chemin est correct
+const setupChat = require('./chatSocket'); // Vérifiez le chemin
+const authRoutes = require('./authRoutes'); // Vérifiez le chemin
+const tournamentRoutes = require('./tournamentRoutes'); // Vérifiez le chemin
 
-const app = express();
-const port = 3001;
-const cors = require('cors');
-app.use(cors()); // Activez CORS pour toutes les requêtes
+// Création du serveur HTTP à partir de l'app Express
+const server = http.createServer(app);
 
-
-// Votre configuration de connexion MySQL existante
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'invvXmpe8',
-  database: 'Lycorn',
-  // Spécifiez le port ici si nécessaire
-});
-
-connection.connect(err => {
-  if (err) {
-    console.error('Erreur de connexion : ' + err.stack);
-    return;
+// Initialisation de socket.io avec le serveur HTTP
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
   }
-
-  console.log('Connecté à la base de données MySQL avec succès.');
 });
 
-// Middleware pour parser le body des requêtes en JSON
-app.use(bodyParser.json());
+// Utilisation des routeurs
+app.use(authRoutes);
+app.use(tournamentRoutes);
 
-app.post('/api/register', (req, res) => {
-  const { name, email, password } = req.body;
-  
-  // Vérifiez d'abord si l'email existe déjà
-  const checkEmailQuery = "SELECT email FROM Utilisateurs WHERE email = ?";
-  
-  connection.query(checkEmailQuery, [email], async (err, results) => {
-    if (err) {
-      return res.status(500).send('Erreur lors de la vérification de l\'email');
-    }
-    
-    if (results.length > 0) {
-      // Si l'email existe déjà, renvoyez une erreur
-      return res.status(409).send('Cette adresse email est déjà utilisée par un autre compte.');
-    } else {
-      // Si l'email n'existe pas, procédez à l'inscription
-      // Hashage du mot de passe avant de l'insérer dans la base de données
-      try {
-        const hashedPassword = await bcrypt.hash(password, saltRounds);
-        const insertQuery = "INSERT INTO Utilisateurs (name, email, password) VALUES (?, ?, ?)";
-        
-        connection.query(insertQuery, [name, email, hashedPassword], (err, result) => {
-          if (err) {
-            return res.status(500).send('Erreur lors de l\'inscription');
-          }
-          res.status(200).send('Utilisateur enregistré avec succès');
-        });
-      } catch (error) {
-        res.status(500).send('Erreur lors du hashage du mot de passe');
-      }
-    }
-  });
-});
+// Configuration de Socket.io pour le chat
+setupChat(io);
 
-app.listen(port, () => {
+const port = 3001;
+server.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
